acec455af2ee1aa38de3bf0aaaf2113bcc06712e
Unchaev Shamil <unchaevss@pochtabank.ru>
2019-05-01 12:45:49 +0300

    print args

--- src/ru/jsam/simple/Main.java
+++ src/ru/jsam/simple/Main.java
@@ -1,8 +1,10 @@
 package ru.jsam.simple;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println(Arrays.asList(args));
     }
 }
