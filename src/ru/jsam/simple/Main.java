c8abe8b314e47c07a6962b10a5984eb2ed9cbad7
Unchaev Shamil <unchaevss@pochtabank.ru>
2019-05-01 13:36:48 +0300

    print args

--- src/ru/jsam/simple/Main.java
+++ src/ru/jsam/simple/Main.java
@@ -1,10 +1,23 @@
 package ru.jsam.simple;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String s1 = scanner.next();
+//        System.out.println("s1 = " + s1);
+//
+//        String s2 = scanner.next();
+//        System.out.println("s2 = " + s2);
+//
+//        String s3 = scanner.next();
+//        System.out.println("s3 = " + s3);
+
+
         System.out.println(Arrays.asList(args));
     }
 }
